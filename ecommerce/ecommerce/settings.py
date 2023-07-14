6c4484a6a3bbf0a61c6ce4f2c558919b89828ebf
Shubhamg111 <guptashuvam111@gmail.com>
2023-07-14 17:48:49 +0545

    adding category and their products

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -127,6 +127,10 @@ MEDIA_URL='/images/'
 
 MEDIA_ROOT=BASE_DIR / 'static'
 
+STATICFILES_DIRS=[
+    BASE_DIR / 'static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
